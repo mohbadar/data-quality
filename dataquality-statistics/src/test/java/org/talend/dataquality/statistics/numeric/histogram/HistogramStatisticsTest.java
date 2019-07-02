d1d4e5a58ae76b244808c88df0d99f934b2bd8db
xywang <xywang@xywang-tlnd.talend.com>
2019-07-02 18:59:23 +0800

    chore(tdq-14238)： Add junit for HistogramStatistics

--- dataquality-statistics/src/test/java/org/talend/dataquality/statistics/numeric/histogram/HistogramStatisticsTest.java
+++ dataquality-statistics/src/test/java/org/talend/dataquality/statistics/numeric/histogram/HistogramStatisticsTest.java
@@ -1,5 +1,7 @@
 package org.talend.dataquality.statistics.numeric.histogram;
 
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -68,5 +70,48 @@ public class HistogramStatisticsTest {
         } catch (DQStatisticsRuntimeException e) {
             Assert.assertEquals("invalid numBins value :-100 , numBins must be a none zero integer", e.getMessage());
         }
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void testSetParameters5() {
+        HistogramStatistics hs = new HistogramStatistics();
+        // Normal case 5 : numBins < 0
+        hs.setParameters(100d, 98d, 1);
+
+        Map<Range, Long> histogram = hs.getHistogram();
+        for (Range r : histogram.keySet()) {
+            Assert.assertEquals("min value:98d", 98.0, r.getLower(), 0.1);
+            Assert.assertEquals("Max value is 100d", 100.0, r.getUpper(), 0.1);
+        }
+        for (Long lo : histogram.values()) {
+            Assert.assertEquals("value is 0", 0.0, lo, 0.1);
+        }
+
+    }
+
+    @Test
+    public void testAdd1() {
+        HistogramStatistics hs = new HistogramStatistics();
+        hs.setParameters(100d, 98d, 2);
+
+        // The added  data is smaller than min
+        hs.add(1d);
+        Assert.assertEquals("1", 1, hs.getCountBelowMin(), 0.1);
+
+        // The added  data is bigger than max
+        hs.add(101d);
+        Assert.assertEquals("1", 1, hs.getCountAboveMax(), 0.1);
+    }
+
+    @Test
+    public void testAdd2() {
+        HistogramStatistics hs = new HistogramStatistics();
+        hs.setParameters(100d, 98d, 2);
+
+        //The added data is smaller than min, and bigger than max
+        hs.add(99d);
+        Assert.assertEquals("0", 0, hs.getCountBelowMin(), 0.1);
+        Assert.assertEquals("0", 0, hs.getCountAboveMax(), 0.1);
     }
 }
